6c048f054632202f9bd4cbe38c6a989280a91822
benjaminangafua <benjaminarkutl2017@gmail.com>
2025-08-05 10:12:27 +0000

    Updated introduction to GirlzApp

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 # Title
 st.title(st.secrets["openai"]["APP_TITLE"])
 st.write("""
-#🌸 GirlzApp+ Chatbot
+# 🌸 GirlzApp+ Chatbot
 ### Welcome to your safe space, Girl!
 I'm here just for you. Ask me anything about your body, your period, your emotions, or your rights.
 Want to track your cycle? Learn about birth control? Understand changes in your body?
